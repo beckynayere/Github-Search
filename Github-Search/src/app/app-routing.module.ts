e110275709c90c270448bafd680f7ca77d5ec97e
becky nayere <rebecca.nayere@gmail.com>
2020-09-02 02:19:41 +0300

    add path to the routing module

--- Github-Search/src/app/app-routing.module.ts
+++ Github-Search/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// tslint:disable-next-line:import-spacing
+import {NotFoundComponent} from './not-found/not-found.component';
+import{RepoListComponent} from './repo-list/repo-list.component';
+import {UserDataComponent} from './user-data/user-data.component';
+
+
+const routes: Routes = [
+  {path: 'user-list', component :UserDataComponent},
+  {path:'repo-list,' , component: RepoListComponent},
+  {path: '', redirectTo:'/user-list' ,pathMatch: 'full'},
+  {path:'**', component : NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
+
+
