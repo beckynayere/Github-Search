b9bcd4a32ce835a201bc3478e564349743c687a9
becky nayere <rebecca.nayere@gmail.com>
2020-09-02 00:56:21 +0300

    add a custom directive

--- Github-Search/src/app/logo-highlight.directive.spec.ts
+++ Github-Search/src/app/logo-highlight.directive.spec.ts
@@ -1,9 +1,8 @@
 import { LogoHighlightDirective } from './logo-highlight.directive';
-import {gitservice}
+
 describe('LogoHighlightDirective', () => {
   it('should create an instance', () => {
     const directive = new LogoHighlightDirective();
     expect(directive).toBeTruthy();
   });
 });
-
--- Github-Search/src/app/logo-highlight.directive.ts
+++ Github-Search/src/app/logo-highlight.directive.ts
@@ -5,23 +5,8 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class LogoHighlightDirective {
 
-  constructor(private elem: ElementRef) { 
+  constructor(private elem: ElementRef) {
     this.elem.nativeElement.style.color = '#51e2f5';
-  }
+   }
 
 }
-
-
-
-// import { Directive, Input, ElementRef } from '@angular/core';
-
-// @Directive({
-//   selector: '[appLogoHighlight]'
-// })
-// export class LogoHighlightDirective {
-
-//   constructor(private elem: ElementRef) {
-//     this.elem.nativeElement.style.color = '#51e2f5';
-//    }
-
-// }
