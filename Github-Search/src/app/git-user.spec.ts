211cf43c989a103894089013bbbbdf68e675b09b
becky nayere <rebecca.nayere@gmail.com>
2020-09-04 19:54:52 +0300

    Auto-generated commit

--- Github-Search/src/app/git-user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GitUser } from './git-user';
-
-describe('GitUser', () => {
-  it('should create an instance', () => {
-    expect(new GitUser()).toBeTruthy();
-  });
-});
--- src/app/logo-highlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LogoHighlightDirective } from './logo-highlight.directive';
-
-describe('LogoHighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LogoHighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
