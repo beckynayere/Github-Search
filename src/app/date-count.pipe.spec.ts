7d16b1d747fdfa39d6ea455036f9b730254251a8
becky nayere <rebecca.nayere@gmail.com>
2020-09-07 16:27:38 +0300

    Auto-generated commit

--- src/app/date-count.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DatepipePipe } from './date-count.pipe';
-
-describe('DateCountPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DatepipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
